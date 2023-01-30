cc0e6eb32f6bfa6db2c5e3abd1e6f2299cb1ff0b
Matheus <matheusmoura2110@gmail.com>
2023-01-30 11:19:20 -0300

    feat: creates BrConnector to fetch products from api

---
+++ app/server/src/api/services/BrConnector.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+class BrConector {
+    static async list() {
+        const getData = async () => {
+            try {
+                const response = await axios.get('http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider');
+                return response.data;
+            } catch (error) {
+                console.error(error);
+                throw error;
+            }
+        }
+        const data = await getData();
+        return this.mapBrData(data);
+    };
+
+    static async getById(id) {
+        const getData = async () => {
+            try {
+                const response = await axios.get(`http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider/${id}`);
+                return response.data;
+            } catch (error) {
+                console.error(error);
+                throw error;
+            }
+        }
+        const data = await getData();
+        return this.mapBrData([data]);
+    };
+
+    static mapBrData(data) {
+        return data.map((item) => ({
+            "nome": item.nome,
+            "descricao": item.descricao,
+            "desconto": '0',
+            "imagem": item.imagem,
+            "preco": item.preco,
+            "material": item.departamento,
+            "id": "BR" + item.id
+        }))
+    }
+};
+
+export { BrConector };
