b6dac103fba04ca533ed68180de926f8691248a9
Matheus <matheusmoura2110@gmail.com>
2023-01-31 01:18:07 -0300

    feat: add a sequelizerc file to correct the paths

--- app/server/src/api/server.js
+++ app/server/src/api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const ProductRouter = require('./routes/ProductRouter');
 
+const seed = require('../database/seeders/20230130183434-orders');
+seed().catch(console.error);
+
 const app = express();
 const port = 3000;
 
