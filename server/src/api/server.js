6e6c5d95af287af1b2a6afb94727ec2a43cafcf9
Matheus Moura <matheusmoura2110@gmail.com>
2023-02-28 11:48:39 -0300

    add swagger and config routes

--- server/src/api/server.js
+++ server/src/api/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const ProductRouter = require('./routes/ProductRouter');
 const OrderRouter = require('./routes/OrdersRouter');
+const swaggerDocs = require('./swagger.json');
+
+
+
+
 
 const app = express();
 
 const port = 3000;
 
 
+const swaggerUi = require('swagger-ui-express');
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+
 app.use(express.json());
 app.use('/products', ProductRouter);
 app.use('/', OrderRouter);
+
+  
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
